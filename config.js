5b2bc801d9de9c43b5a5334b7c81a5e80a0fbbb8
KAVI-BOTCREATE <kavindudilsharawirasiri@gmail.com>
2024-11-12 07:48:33 +0000

    𝐀𝐋𝐏𝐇𝐀 - 𝐕0 🧬💗

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    SESSION_ID: 'YOUR_MEGA_SESSION_ID_HERE',  // Mega.nz Session ID එක මෙහි දාන්න
+    ownerNumber: ['94704467936'],             // ඔබගේ අංකය මෙහි දැමීම
+    prefix: '.',                              // Bot එකේ prefix එක
+};
